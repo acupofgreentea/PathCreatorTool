c4a863b1ed6a27915490c2e899e7b2b5d3be02a6
Akın <aknyldrm361@gmail.com>
2023-03-29 00:28:44 +0300

    Insert added to tool

--- Assets/Scripts/Editor/PathCreatorTool.cs
+++ Assets/Scripts/Editor/PathCreatorTool.cs
@@ -12,12 +12,16 @@ public class PathCreatorTool : EditorWindow
         GetWindow<PathCreatorTool>(false, "Path Creator");
     }
 
-    public List<Transform> PathPoints = new List<Transform>();
-    private SerializedProperty PathPointsProp;
-    private SerializedProperty TemplateBaseProp;
+    public List<Transform> PathPoints;
+    private SerializedProperty pathPointsProp;
+    private SerializedProperty isInsertProp;
+    private SerializedProperty increaseInstertIndex;
     private Transform parent;
     private SerializedObject serializedObject;
     private PathCreatorController pathCreatorController;
+    private int insertIndex;
+    public bool IsInsert;
+    public bool IncreaseInsertIndex;
 
     private PathManager pathManager;
     public GameObject TemplateBase;
@@ -27,8 +31,9 @@ public class PathCreatorTool : EditorWindow
         serializedObject ??= new SerializedObject(this);
         pathCreatorController ??= new PathCreatorController();
 
-        PathPointsProp = serializedObject.FindProperty("PathPoints");
-        TemplateBaseProp = serializedObject.FindProperty("TemplateBase");
+        pathPointsProp = serializedObject.FindProperty("PathPoints");
+        isInsertProp = serializedObject.FindProperty("IsInsert");
+        increaseInstertIndex = serializedObject.FindProperty("IncreaseInsertIndex");
         SceneView.duringSceneGui += SceneGUI; 
         
         CreateTemplateBase();
@@ -72,41 +77,65 @@ public class PathCreatorTool : EditorWindow
         
         GUILayout.Space(15);
         
-        EditorGUILayout.PropertyField(PathPointsProp);
+        EditorGUILayout.PropertyField(pathPointsProp);
         
         GUILayout.Space(15);
 
         parent = (Transform)EditorGUILayout.ObjectField("Parent", parent, typeof(Transform), true);
         
+        GUILayout.Space(15);
+
+        EditorGUILayout.PropertyField(isInsertProp);
+        
+        if(IsInsert)
+        {
+            GUILayout.Space(15);
+            GUILayout.Label("Press 'I' to Insert Cube.", centeredStyle);
+            EditorGUILayout.PropertyField(increaseInstertIndex);
+            insertIndex = EditorGUILayout.IntField("Insert Index", insertIndex);
+        }
+        
         GUILayout.FlexibleSpace();
 
+        if (GUILayout.Button("Reverse"))
+        {
+            Reverse();    
+        }
+        
+        if (GUILayout.Button("Draw Path"))
+        {
+            DrawPath();
+        }
+        
         if (GUILayout.Button("Clear"))
         {
-            if (parent == null)
-            {
-                Debug.LogError("Parent is not assigned!");
-                return;
-            }
-            
-            PathPoints.Clear();
-            pathManager.ClearPath();
+            Clear();
         }
 
         if (GUILayout.Button("Destroy Cubes"))
         {
-            List<Transform> cubes = PathPoints;
+            Transform[] cubes = parent.GetComponentsInChildren<Transform>();
 
             foreach (Transform transform in cubes)
             {
+                if(transform == parent)
+                    continue;
+                
                 DestroyImmediate(transform.gameObject);
             }
             
-            PathPoints.Clear();
-            pathManager.ClearPath();
+            Clear();
         }
         
-        
+        Repaint();
         serializedObject.ApplyModifiedProperties();
+    }
+
+    private void Clear()
+    {
+        insertIndex = 0;
+        PathPoints.Clear();
+        pathManager.ClearPath();
     }
 
     private void SceneGUI(SceneView sceneView)
@@ -116,15 +145,61 @@ public class PathCreatorTool : EditorWindow
            SpawnCube();
         }
         
+        if(Event.current.type == EventType.KeyDown && (Event.current.character == 'i' || Event.current.character == 'ı'))
+        {
+            InsertCube(insertIndex);
+        }
+        
+        
         Repaint();
     }
+    
+    private void Reverse()
+    {
+        if (PathPoints.Count == 0)
+            return;
+        
+        PathPoints.Reverse();
+        
+        DrawPath();
 
+        serializedObject.ApplyModifiedProperties();
+    }
+    
     private void DrawPath()
     {
         pathManager.PathPoints = PathPoints;
         pathManager.GeneratePath();
     }
 
+    private void InsertCube(int index)
+    {
+        Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+            
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+        {
+            GameObject cube = pathCreatorController.SpawnPathPoint();
+
+            cube.transform.position = hit.point;
+            cube.transform.parent = parent;
+            PathPoints.Insert(index, cube.transform);
+        }
+
+        insertIndex = PathPoints.Count;
+        DrawPath();
+        ReOrderCubes();
+    }
+
+    private void ReOrderCubes()
+    {
+        for (var i = 0; i < PathPoints.Count; i++)
+        {
+            PathPoints[i].transform.SetSiblingIndex(i);
+            
+            PathPoints[i].gameObject.name = "Cube" + "_" + i;
+        }
+    }
+    
     private void SpawnCube()
     {
         Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
@@ -132,10 +207,14 @@ public class PathCreatorTool : EditorWindow
         if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
         {
             GameObject cube = pathCreatorController.SpawnPathPoint();
+
+            cube.gameObject.name = "Cube" + "_" + PathPoints.Count;
             cube.transform.position = hit.point;
             cube.transform.parent = parent;
             PathPoints.Add(cube.transform);
         }
+
+        insertIndex = PathPoints.Count;
         
         DrawPath();
     }
---
+++ Assets/Scripts/PathFollowerEditMode.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using PathCreation;
+using UnityEngine;
+
+[ExecuteAlways]
+public class PathFollowerEditMode : MonoBehaviour
+{
+    [SerializeField] private float distanceTravelled;
+    [SerializeField] private float speed = 5f;
+
+    [SerializeField] private bool canMove = true;
+
+    [SerializeField] private PathCreator pathCreator;
+
+    private void Update()
+    {
+        if (pathCreator == null || !canMove)
+            return;
+        
+        distanceTravelled += Time.deltaTime * speed;
+
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+        transform.forward = pathCreator.path.GetDirectionAtDistance(distanceTravelled);
+    }
+
+    void OnDrawGizmos()
+    {
+        // Your gizmo drawing thing goes here if required...
+
+#if UNITY_EDITOR
+        // Ensure continuous Update calls.
+        if (!Application.isPlaying)
+        {
+            UnityEditor.EditorApplication.QueuePlayerLoopUpdate();
+            UnityEditor.SceneView.RepaintAll();
+        }
+#endif
+    }
+}
